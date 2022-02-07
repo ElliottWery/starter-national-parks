69a18cc1d421494f8706b7fce3b1f912c4085bfd
ElliottWery <92320166+ElliottWery@users.noreply.github.com>
2022-02-07 12:30:31 -0800

    changes

--- src/index.js
+++ src/index.js
@@ -52,3 +52,146 @@ const park = main.querySelector(".park-display");
 
 // Remove that park
 main.removeChild(park);
+
+
+const firstBtn = document.querySelector("button");
+firstBtn.addEventListener("click", (event) => {
+  console.log("You clicked the button", event);
+});
+
+const allBtns = document.querySelectorAll(".rate-button");
+
+// Iterate through the list of buttons and add an event handler to each
+allBtns.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    console.log(event.target.parentNode);
+  });
+});
+
+
+
+// Select the `nameSorter` link
+const nameSorter = document.querySelector("#name-sorter");
+
+// Add an event listener
+nameSorter.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  // 1.  Get the main element
+  const main = document.querySelector("main");
+
+  // 2. Get the list of parks
+  const parksList = main.querySelectorAll(".park-display");
+
+  // 3. Empty the main element
+  main.innerHTML = "";
+});
+
+const parksArray = Array.from(parksList);
+
+parksArray.sort((parkA, parkB) => {
+  const parkAName = parkA.querySelector("h2").innerText;
+  const parkBName = parkB.querySelector("h2").innerText;
+  if (parkAName < parkBName) {
+    return -1;
+  } else if (parkAName > parkBName) {
+    return 1;
+  } else {
+    return 0;
+  }
+});
+
+
+//equivalent to the code above............................... 
+const sortByName = (parkA, parkB) => {
+  const parkAName = parkA.querySelector("h2").innerText;
+  const parkBName = parkB.querySelector("h2").innerText;
+  if (parkAName < parkBName) {
+    return -1;
+  } else if (parkAName > parkBName) {
+    return 1;
+  } else {
+    return 0;
+  }
+};
+
+// Function for handling the `nameSorter` click
+const nameSorterClickHandler = (event) => {
+  event.preventDefault();
+
+  // 1.  Get the main element
+  const main = document.querySelector("main");
+
+  // 2. Get the list of parks
+  const parksList = main.querySelectorAll(".park-display");
+
+  // 3. Empty the main
+  main.innerHTML = "";
+
+  // 4. Create an array
+  const parksArray = Array.from(parksList);
+
+  // 5. Sort the array
+  parksArray.sort(sortByName);
+
+  // 6. Insert each park into the DOM
+  parksArray.forEach((park) => {
+    main.appendChild(park);
+  });
+};
+
+// Select the `nameSorter` link
+const nameSorter = document.querySelector("#name-sorter");
+
+// Add an event listener
+nameSorter.addEventListener("click", nameSorterClickHandler);
+
+
+
+
+//___________________________________________________________________
+console.log("Before!");
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  console.log("Loaded!");
+});
+
+console.log("After!");
+
+// Declare handler and support functions here
+
+// Function for sorting by name
+const sortByName = ...
+
+// Function for sorting by rating
+const sortByRating = ...
+
+// Function for handling the `nameSorter` click
+const nameSorterClickHandler = ...
+
+// Function to handle the `ratingSorter` click
+const ratingSorterClickHandler = ...
+
+
+// The code that runs once the DOM is loaded
+const main = () => {
+  // Select the `nameSorter` link
+  const nameSorter = document.querySelector("#name-sorter");
+
+  // Add an event listener
+  nameSorter.addEventListener("click", nameSorterClickHandler);
+
+  // Select the `ratingSorter` link
+  const ratingSorter = document.querySelector("#rating-sorter");
+
+  // Add an event listener
+  ratingSorter.addEventListener("click", ratingSorterClickHandler);
+}
+
+// Add event listener for `DOMContentLoaded`
+window.addEventListener("DOMContentLoaded", main);
+
+
+
+
+
